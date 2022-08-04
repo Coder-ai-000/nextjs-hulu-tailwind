6607a72f6fb094c7c31f7b09d99995caeeddb60b
Saleh <saleh.alebi@gmail.com>
2022-08-04 11:02:37 -0400

    just my MovieGenreHook after adding my extractor

--- hooks/useFetchMovieGenreResults.js
+++ hooks/useFetchMovieGenreResults.js
@@ -1,77 +1,71 @@
-import { object } from "joi";
 import React from "react";
+import Moviegenreresults from "../components/Moviegenreresults";
 import useFetchNavBarCatagories from "./useFetchNavBarCatagories";
+import { useEffect } from "react";
 
-export default function useFetchMovieGenreResults(x) {
+//! Make a note below that hooks take in like functions, what is taken in is an argument, not a prop (whitch components take in and may need to be deconstructed for easy use)
+
+export default function useFetchMovieGenreResults(genre) {
   //warnning here this fetch does not have trending and top rated
+
+  console.log("Checking if its reading", genre);
+
   const mymoviegenreobjects = useFetchNavBarCatagories();
 
-  function filterByPosition(array, number, position) {
-    return array.filter((innerArray) => innerArray[position - 1] !== number);
+  if (mymoviegenreobjects.genres.length > 0) {
+    console.log(
+      "This is what I get back from my UseNavBarCatagories hook",
+      mymoviegenreobjects
+    );
+
+    ///***************************************************** */
+
+    // const asArray = Object.entries(mymoviegenreobjects);
+    // console.log(
+    //   "This is me logging my object keys " + Object.keys(mymoviegenreobjects)
+    // );
+
+    // console.log(
+    //   "This is me logging my object values " +
+    //     Object.values(mymoviegenreobjects)
+    // );
+
+    ///***************************************************** */
+
+    //! The bottom maps the key's values {genre[]}. Basically removing the outter object
+    var mappedvalues = Object.keys(mymoviegenreobjects).map(
+      (e) => mymoviegenreobjects[e]
+    );
+
+    console.log("The latest testinggggggggggggggggggg", mappedvalues);
+
+    ///***************************************************** */
+
+    // console.log("This is me logging my object in OG form", mymoviegenreobjects);
+
+    // const valuesasArray = Object.entries(values);
+    // console.log("These are my values as arrays", valuesasArray);
+    // const filteringvaluesasArray = values.filter((num) => num !== 0);
+    // console.log(
+    //   "Leaving only my genre values in as array",
+    //   filteringvaluesasArray
+    // );
+
+    // const justafiltertest = filterByPosition(filteringvaluesasArray, 3, 1);
+    // console.log("ello", justafiltertest);
+
+    ///***************************************************** */
+
+    const flatarrayofvalues = mappedvalues.flat();
+    console.log("Finally flat", flatarrayofvalues);
+
+    const filtteredarray = flatarrayofvalues.filter(
+      (character) => character.name === genre
+    );
+
+    console.log("This is avangers two ID", filtteredarray[0].id);
+    console.log("This is avangers two name name", filtteredarray[0].name);
+
+    return filtteredarray;
   }
-
-  const asArray = Object.entries(mymoviegenreobjects);
-  console.log(
-    "This is me logging my object keys " + Object.keys(mymoviegenreobjects)
-  );
-
-  console.log(
-    "This is me logging my object values " + Object.values(mymoviegenreobjects)
-  );
-
-  var values = Object.keys(mymoviegenreobjects).map(
-    (e) => mymoviegenreobjects[e]
-  );
-
-  console.log("The latest test", values);
-
-  console.log("This is me logging my object in OG form", mymoviegenreobjects);
-  console.log("This is me logging my object as an array", asArray);
-  console.log("The latest test", values);
-
-  // const valuesasArray = Object.entries(values);
-  // console.log("These are my values as arrays", valuesasArray);
-  const filteringvaluesasArray = values.filter((num) => num !== 0);
-  // console.log(
-  //   "Leaving only my genre values in as array",
-  //   filteringvaluesasArray
-  // );
-
-  const justafiltertest = filterByPosition(filteringvaluesasArray, 3, 1);
-  console.log("ello", justafiltertest);
-
-  const flatNumbers = justafiltertest.flat(2);
-  console.log("Finally flat", flatNumbers);
-
-  const avengers = flatNumbers.filter(
-    (character) => character.name === "Adventure"
-  );
-
-  const avengerstwo = avengers[0];
-
-  // console.log("This is avengers", avengers);
-  // console.log("hello trying bracket notation", avengers);
-
-  // const avengerstwo = avengers[0]
-
-  // console.log("This is avengerstwo", avengerstwo);
-
-  // Object.entries(avengers);
-
-  // var keyz = Object.values(avengers);
-  // console.log("maoi", keyz);
-
-  //   console.log(Object.values("This is my keys", asArray));
-
-  //   const chosengenre = mymoviegenreobjects.filter(
-  //     (mygenre) => (mygenre.name = x.name)
-  //   );
-
-  //   console.log(filtered);
-
-  console.log("This is avangers two", avengerstwo);
-
-  //WHAT I WANT TO DO IS PUT AVANGERS.ID into a variable called ID and AVENGERS.NAME into a variable called name//
-
-  return avengerstwo;
 }
